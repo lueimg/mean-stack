500e70209b23d89aad8f0e8417dda19cd572caa0
luis mori <luismorith@gmail.com>
2014-12-08 17:58:59 -0500

    angular usando localstoge

--- tareas/app/scripts/controllers/main.js
+++ tareas/app/scripts/controllers/main.js
@@ -8,12 +8,20 @@
  * Controller of the tareasApp
  */
 angular.module('tareasApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.tareas = [ ];
+  .controller('MainCtrl', function ($scope,localStorageService) {
+    
+
+    var tareasEnAlmacen = localStorageService.get('tareas');
+    //agregamos las tareas que existan en memoria
+    $scope.tareas = tareasEnAlmacen && tareasEnAlmacen.split('\n') || [];
+    //syncroniza el localstorage cuando se agrege algo a tareas
+    $scope.$watch('tareas',function(){
+    	localStorageService.add('tareas', $scope.tareas.join('\n'));
+    },true);
 
 	$scope.addTarea = function(){
 			$scope.tareas.push($scope.tarea);
-			$scope.tarea = ''
+			$scope.tarea = '';
 	}
 
 	$scope.eliminarTarea = function(index){
