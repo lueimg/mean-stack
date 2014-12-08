1f4a48719eaae1e14b22e366bae8af9235ea41b3
luis mori <luismorith@gmail.com>
2014-12-08 14:41:13 -0500

    usando grunt

--- miproyecto/gruntfile.js
+++ miproyecto/gruntfile.js
@@ -4,10 +4,26 @@ module.exports = function(grunt){
 
 		jshint:{
 			all:['script.js']
+		},
+		concat:{
+			dist:{
+				src:['script.js','otroscript.js'],
+				dest:'unidos.js'
+			}
+		},
+		uglify:{
+			dist:{
+				src:'unidos.js',
+				dest:'build/unidos.min .js'
+			}
 		}
+
+
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.registerTask('default',['jshint']);
+	grunt.registerTask('default',['jshint','concat','uglify']);
 }
