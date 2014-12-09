a1800ea35f624ed10c536cc60ab7daa797508c91
luis mori <luismorith@gmail.com>
2014-12-08 19:24:59 -0500

    CAROUSEL angular bootstrap

--- prototipo/app/scripts/controllers/main.js
+++ prototipo/app/scripts/controllers/main.js
@@ -13,4 +13,22 @@ angular.module('prototipoApp')
   	var baseURL = 'http://lorempixel.com/960/450/';
   	$scope.setInterval = 5000;
 
+  	$scope.slides = [
+  		{
+  			title:'aprende a mantenerte en forma',
+  			image: baseURL + 'sports/',
+  			text:'Praactica algundeporte todos los dias'
+  		},
+  		{
+  			title:'aBUena alimentacion',
+  			image: baseURL + 'food/',
+  			text:'Praactica algundeporte todos los dias'
+  		},
+  		{
+  			title:'apEn contacot con la naturalezama',
+  			image: baseURL + 'nature/',
+  			text:'Praactica algundeporte todos los dias'
+  		}
+  	];
+
   });
